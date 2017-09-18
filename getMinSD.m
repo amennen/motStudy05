34a098ceae142e3aefa88c0b023710d32af6342a
Anne Mennen <amennen@princeton.edu>
2017-09-18 12:18:56 -0400

    check sd

--- getMinSD.m
+++ getMinSD.m
@@ -2,7 +2,7 @@ nrun = 3;
 for i = 1:nrun
   
     folder = ['motRun' num2str(i)];
-    cd(i);
+    cd(folder);
     fn = dir([lastRunHeader 'motpatternsdata_' '*']);
     n = load(fullfile(lastRunHeader,fn(end).name));
     sd = n.patterns.runStd;
