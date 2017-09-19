7e1336c9ba89c615b8bd20505cd714d72926084d
Anne Mennen <amennen@princeton.edu>
2017-09-18 22:31:22 -0400

    bad vox

--- findbadvoxels.m
+++ findbadvoxels.m
@@ -1,17 +1,17 @@
+% check if in sigvox
+r = load('reg/retrieval_mask.mat');
+roiInds = find(r.mask_brain);
+fn = dir(['Localizer/locpatternsdata_' '*']);
+n = load(fullfile(fn(end).name));
+ind = roiInds(n.patterns.sigVox);
+
 for i = 1:3
     folder = ['motRun' num2str(i)];
     cd(folder);
     fn = dir(['motpatternsdata_' '*']);
     n = load(fullfile(fn(end).name));
     
-    test_sd = std(n.patterns.raw(1:iTrial,:),[],1);
-    bad = find(test_sd==0 & n.patterns.raw(iTrial,:)<20);
+    test_sd = std(n.patterns.raw(:,:),[],1);
+    bad = find(test_sd==0);% & n.patterns.raw(iTrial,:)<20);
     cd ../
-        %z(iTrial) = length(bad);
-        
-        %if ~isempty(bad)
-        %    notMoving = [notMoving bad];
-        %    patterns.constantVoxels = unique(notMoving); % MKAE THIS VALUES NONT INDECIES!!!
-        %   patterns.raw_sm_filt_z(iTrial,patterns.constantVoxels) = 0;
-        %end
 end
