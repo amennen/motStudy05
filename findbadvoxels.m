726218e2b7db1558ff075ae57d59daa3053e91f1
Anne <amennen@princeton.edu>
2017-09-20 11:59:59 -0400

    s30 day 2

--- findbadvoxels.m
+++ findbadvoxels.m
@@ -1,14 +1,12 @@
 % check if in sigvox
+addpath('/Data1/code/motStudy05/code/')
 r = load('reg/retrieval_mask.mat');
 roiInds = find(r.mask_brain);
 
 fname = findNewestFile('Localizer',fullfile(['Localizer/locpatternsdata'  '*.mat']));
-%load(fname);
-
-fn = dir(['Localizer/locpatternsdata_' '*']);
-n = load(fullfile(fn(end).name));
-ind = roiInds(n.patterns.sigVox);
-
+n = load(fname);
+indwholething = roiInds(n.patterns.sigVox);
+ind = n.patterns.sigVox;
 for i = 1:3
     folder = ['motRun' num2str(i)];
     cd(folder);
@@ -17,5 +15,8 @@ for i = 1:3
     
     test_sd = std(n.patterns.raw(:,:),[],1);
     bad = find(test_sd==0);% & n.patterns.raw(iTrial,:)<20);
+    i
+    bad
     cd ../
 end
+ismember(bad,ind)
