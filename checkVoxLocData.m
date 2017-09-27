097aa9a8212e404f28ad9f010bafdb9047b61f8d
Anne Mennen <amennen@princeton.edu>
2017-09-27 08:42:03 -0400

    loc data

---
+++ checkVoxLocData.m
@@ -0,0 +1,12 @@
+% so you have the run, bad voxel number
+folder = ['../data/' num2str(subject) '/Localizer']
+fname = findNewestFile(folder,fullfile(folder,['loctrainedModel'  '*.mat']));
+p = load(fname);
+weights = p.trainedModel.ridge.betas;
+fname = findNewestFile(folder,fullfile(folder,['locpatterns'  '*.mat']));
+p = load(fname);
+sigVox = p2.patterns.sigVox;
+for i = 1:length(z.patterns.allLow)
+    
+    
+end
