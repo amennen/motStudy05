e3f8c1715b3ffe69fbcd25f61b14c16f65410fb3
Anne <amennen@princeton.edu>
2017-10-06 12:29:56 -0400

    s34 and 35 day 2

--- ProcessNiftiMask.m
+++ ProcessNiftiMask.m
@@ -29,10 +29,10 @@ setenv('FSLOUTPUTTYPE','NIFTI_GZ');
 % inputs (eventually function)
 
 
-subjNum = 31;
+subjNum = 34;
 %subjDate = '4-5-17';
 subjDate = NaN;
-runNum = 1;
+runNum = 2;
 
 
 highresScan = 5;
