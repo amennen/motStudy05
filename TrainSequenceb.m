b8286a81d1daeb53acc38ee38bb3a4928115a771
amennen <amennen@princeton.edu>
2017-10-17 11:23:43 -0400

    s38

--- TrainSequenceb.m
+++ TrainSequenceb.m
@@ -2,10 +2,10 @@ base_path = [fileparts(which('mot_realtime05.m')) filesep];
 cd(base_path);
 
 
-SUBJECT = 37;
+SUBJECT = 39;
 subjDir = [base_path 'BehavioralData' filesep num2str(SUBJECT) filesep];
 
-SVEC = [19 23 29]; % 6 is used--should add check no used are here and 11 is yoked!
+SVEC = [19]; % 6 is used--should add check no used are here and 11 is yoked!
 
 NUM_TASK_RUNS = 3;
 % orientation session
