4d96bcdc3cd75b933c300a9523c5801bf6ed5c83
Anne <amennen@princeton.edu>
2017-10-18 11:50:34 -0400

    38b

--- TrainSequenceb.m
+++ TrainSequenceb.m
@@ -2,7 +2,7 @@ base_path = [fileparts(which('mot_realtime05.m')) filesep];
 cd(base_path);
 
 
-SUBJECT = 39;
+SUBJECT = 38;
 subjDir = [base_path 'BehavioralData' filesep num2str(SUBJECT) filesep];
 
 SVEC = [19]; % 6 is used--should add check no used are here and 11 is yoked!
